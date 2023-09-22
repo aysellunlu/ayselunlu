b0b0493489375652b5a7c6488aaee706812df2d5
Aysel Ünlü <aysell.unlu48@gmail.com>
2023-09-22 22:37:48 +0300

    starter - main area - profile pic

--- src/pages/LandPage/index.jsx
+++ src/pages/LandPage/index.jsx
@@ -2,6 +2,7 @@ import Contact from "../../components/Contact";
 import InfoCard from "../../components/InfoCard";
 import LetsWork from "../../components/LetsWork";
 import styles from "./styles.module.css";
+import ProfileImg from "../../img/profile-img.png";
 
 const LandPage = ({ className = "", ...props }) => {
   return (
@@ -12,7 +13,18 @@ const LandPage = ({ className = "", ...props }) => {
             <span className={`${styles.gray}`}> Hi I'm</span>{" "}
             <span className={`${styles.yellow}`}>Aysel</span>
           </div>
-          <div className={`${styles.text}`}></div>
+          <div className={`${styles.paragraphText} ${styles.gray}`}>
+            I design and code mobile and web interfaces.I worked with different
+            teams at different institutions in the development and design of 10+
+            projects. Currently, as a freelancer, I{" "}
+            <span className={`${styles.pink}`}>design</span> and{" "}
+            <span className={`${styles.pink}`}> develop applications </span>{" "}
+            that bring <span className={`${styles.blue}`}>creative ideas</span>{" "}
+            to life.
+          </div>
+        </div>
+        <div className={`${styles.mainImg}`}>
+          <img src={ProfileImg} width="50%" />
         </div>
       </div>
       <div className={`${styles.row}`}>
