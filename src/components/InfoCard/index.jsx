916081e7bd8c00b1bca71f961539f9e1af68b5ba
Aysel Ünlü <aysell.unlu48@gmail.com>
2023-09-26 20:50:21 +0300

    info cards

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -9,7 +9,9 @@ const InfoCard = ({
 }) => {
   return (
     <div className={`${styles.card}  ${styles[type]} ${className}`} {...props}>
-      <div className={`${styles.title}`}>{children}</div>
+      <div className={`${styles.title}`}>
+        <div className={`${styles.infoBackground}`}>{children}</div>
+      </div>
     </div>
   );
 };
